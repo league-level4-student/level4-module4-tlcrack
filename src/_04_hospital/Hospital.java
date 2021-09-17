1a23e61d29f7b974916872fa4787359a407f0bd2
League Student <league@iMac-55.attlocal.net>
2021-09-16 17:35:19 -0700

    Hospital, finished module4

--- src/_04_hospital/Hospital.java
+++ src/_04_hospital/Hospital.java
@@ -23,5 +23,19 @@ public class Hospital {
 	ArrayList<Patient> getPatients() {
 		return Patients;
 	}
-	
+	void assignPatientsToDoctors() throws DoctorFullException {
+		for(int i = 0; i < Patients.size(); i++) {
+			boolean isPatientAssigned = false;
+			for(int j = 0; j < Doctors.size(); j++) {
+				if(Doctors.get(j).Patients.size()<3) {
+					Doctors.get(j).assignPatient(Patients.get(i));
+					isPatientAssigned=true;
+					break;
+				}
+			}
+			if(!isPatientAssigned) {
+				System.out.println("We are experiencing an unusually high call frequency right now. Please assign this patient later.");
+			}
+		}
+	}
 }
